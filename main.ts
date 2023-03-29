e68c70bb7fd91d37ee515310dc0f10c8a35b0c56
FUF1 <69962736+FUF1@users.noreply.github.com>
2023-03-29 07:08:04 +0200

    perfectly working with Data Streamer in Excel

--- main.ts
+++ main.ts
@@ -1,8 +1,21 @@
-let RawPulse = 0
+let Pulse = 0
+let GSR = 0
+let Breathing = 0
+music.playTone(262, music.beat(BeatFraction.Whole))
+serial.redirectToUSB()
+serial.setBaudRate(BaudRate.BaudRate9600)
 basic.forever(function () {
-    RawPulse = pins.analogReadPin(AnalogPin.P2)
-    led.plotBarGraph(
-    RawPulse,
-    500
-    )
+    Pulse = pins.analogReadPin(AnalogPin.P0)
+    GSR = pins.analogReadPin(AnalogPin.P1)
+    Breathing = pins.analogReadPin(AnalogPin.P2)
+    dataStreamer.writeNumberArray([
+    Pulse,
+    GSR,
+    Breathing
+    ])
+    serial.writeNumbers([
+    Pulse,
+    GSR,
+    Breathing
+    ])
 })
